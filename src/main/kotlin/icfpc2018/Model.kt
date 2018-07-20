f52ffdd4d3c659c3835f13b367a55a10052b71c6
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2018-07-20 20:03:51 +0300

    Model, episode 2

--- src/main/kotlin/icfpc2018/Model.kt
+++ src/main/kotlin/icfpc2018/Model.kt
@@ -1,2 +1,40 @@
 package icfpc2018
 
+import icfpc2018.util.BitInputStream
+import org.pcollections.HashTreePSet
+import org.pcollections.MapPSet
+import java.io.InputStream
+
+data class Model(val data: MapPSet<Long> = HashTreePSet.empty()) {
+    operator fun get(x: Int, y: Int, z: Int): Boolean = data.contains(convertCoordinates(x,y,z))
+
+    fun set(x: Int, y: Int, z: Int, value: Boolean = true) = when(value) {
+        true -> copy(data = data + convertCoordinates(x,y,z))
+        false -> copy(data = data - convertCoordinates(x,y,z))
+    }
+
+    companion object {
+        private inline fun convertCoordinates(x: Int, y: Int, z: Int) = x + 256L * y + 256L * 256L * z
+
+        fun readMDL(bs: InputStream): Model {
+            val size = bs.read()
+
+            val stream = BitInputStream(bs)
+            var data = HashTreePSet.empty<Long>()
+
+            for(ix in 0..size) {
+                for(iy in 0..size) {
+                    for(iz in 0..size) {
+                        val bit = stream.readBits(1)
+
+                        if(bit != 0) data = data + convertCoordinates(ix, iy, iz)
+                    }
+                }
+            }
+            return Model(data)
+        }
+    }
+
+}
+
+
