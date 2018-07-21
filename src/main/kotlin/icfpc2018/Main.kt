f0e4df4881b3269a93512bf63971c82ad9f8f717
DeveloperHacker <hacker.mad.cat@gmail.com>
2018-07-21 12:03:55 +0300

    solution apply to solve

--- src/main/kotlin/icfpc2018/Main.kt
+++ src/main/kotlin/icfpc2018/Main.kt
@@ -1,41 +1,33 @@
 package icfpc2018
 
-import icfpc2018.bot.commands.Command
 import icfpc2018.bot.state.*
 import icfpc2018.bot.util.persistentTreeSetOf
-import icfpc2018.solutions.trace.Trace
+import icfpc2018.solutions.sections.Sections
 import java.io.File
 import java.io.FileOutputStream
 
 fun main(args: Array<String>) {
-    for (i in 1..186) {
-
+    for (i in 1..1) {
         val currFileName = "LA%03d".format(i)
 
         val targetModelFile = File("models/${currFileName}_tgt.mdl").inputStream()
         val targetModel = Model.readMDL(targetModelFile)
 
         val model = Model(targetModel.size)
-
         val bot = Bot(1, Point(0, 0, 0), (2..20).toSortedSet())
-
         val state = State(0, Harmonics.LOW, model, persistentTreeSetOf(bot))
-
         val system = System(state)
 
-        val traceFile = File("traces/$currFileName.nbt").inputStream()
-        val commands: MutableList<Command> = mutableListOf()
-        while (traceFile.available() != 0) {
-            commands += Command.read(traceFile)
+        val solution = Sections(targetModel, system)
+
+        solution.solve()
+
+        println("Energy = ${system.currentState.energy}")
+        val success = system.currentState.matrix == targetModel
+        println(if (success) "Success" else "Fail")
+        if (success) {
+            val ofile = FileOutputStream(File("solutions/sections/$currFileName.nbt"))
+            system.commandTrace.forEach { it.write(ofile) }
         }
-
-        val solution = Trace(commands)
-
-        solution.apply(system)
-
-        val ofile = FileOutputStream(File("traces/$currFileName.my.nbt"))
-        system.commandTrace.forEach { it.write(ofile) }
-
-        println(system.currentState.matrix == targetModel)
     }
 }
