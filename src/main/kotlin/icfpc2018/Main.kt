cb030377f39ea21706989003796a59eca3a86b49
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2018-07-21 12:10:31 +0300

    Energy -> Long, cli parser

--- src/main/kotlin/icfpc2018/Main.kt
+++ src/main/kotlin/icfpc2018/Main.kt
@@ -1,5 +1,6 @@
 package icfpc2018
 
+import com.xenomachina.argparser.ArgParser
 import icfpc2018.bot.commands.Command
 import icfpc2018.bot.state.*
 import icfpc2018.bot.util.persistentTreeSetOf
@@ -7,7 +8,13 @@ import icfpc2018.solutions.trace.Trace
 import java.io.File
 import java.io.FileOutputStream
 
+class Arguments(parser: ArgParser) {
+    val models by parser.positionalList("Model files", 0..Int.MAX_VALUE)
+}
+
 fun main(args: Array<String>) {
+    val arguments = ArgParser(args).parseInto(::Arguments)
+
     for (i in 1..186) {
 
         val currFileName = "LA%03d".format(i)
@@ -36,6 +43,7 @@ fun main(args: Array<String>) {
         val ofile = FileOutputStream(File("traces/$currFileName.my.nbt"))
         system.commandTrace.forEach { it.write(ofile) }
 
-        println(system.currentState.matrix == targetModel)
+        log.info { "Correct: " + (system.currentState.matrix == targetModel) }
+        log.info { "Energy: " + system.currentState.energy }
     }
 }
