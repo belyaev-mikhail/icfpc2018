a57bac2989bd2640f1655e44527f4a522b37de82
Azat Abdullin <azat.aam@gmail.com>
2018-07-21 13:09:23 +0300

    cmd arguments + results storing

--- src/main/kotlin/icfpc2018/Main.kt
+++ src/main/kotlin/icfpc2018/Main.kt
@@ -6,36 +6,54 @@ import icfpc2018.bot.util.persistentTreeSetOf
 import icfpc2018.solutions.trace.Trace
 import java.io.File
 import java.io.FileOutputStream
+import java.io.StringReader
 
 fun main(args: Array<String>) {
-    for (i in 1..186) {
+    val arguments = Arguments(args)
+    val targetModelName = arguments.getValue("model") ?: throw IllegalArgumentException()
+    val solutionName = arguments.getValue("solution") ?: throw IllegalArgumentException()
 
-        val currFileName = "LA%03d".format(i)
-
-        val targetModelFile = File("models/${currFileName}_tgt.mdl").inputStream()
-        val targetModel = Model.readMDL(targetModelFile)
-
-        val model = Model(targetModel.size)
-
-        val bot = Bot(1, Point(0, 0, 0), (2..20).toSortedSet())
-
-        val state = State(0, Harmonics.LOW, model, persistentTreeSetOf(bot))
-
-        val system = System(state)
-
-        val traceFile = File("traces/$currFileName.nbt").inputStream()
-        val commands: MutableList<Command> = mutableListOf()
-        while (traceFile.available() != 0) {
-            commands += Command.read(traceFile)
-        }
-
-        val solution = Trace(commands)
-
-        solution.apply(system)
-
-        val ofile = FileOutputStream(File("traces/$currFileName.my.nbt"))
-        system.commandTrace.forEach { it.write(ofile) }
-
-        println(system.currentState.matrix == targetModel)
+    val resultsFile = File("results/results.json")
+    val resultReader = when {
+        resultsFile.exists() -> resultsFile.reader()
+        else -> StringReader("{}")
     }
+    val results = Results.fromJson(resultReader)
+
+    val targetModelFile = File("models/${targetModelName}_tgt.mdl").inputStream()
+    val targetModel = Model.readMDL(targetModelFile)
+
+    val model = Model(targetModel.size)
+
+    val bot = Bot(1, Point(0, 0, 0), (2..20).toSortedSet())
+
+    val state = State(0, Harmonics.LOW, model, persistentTreeSetOf(bot))
+
+    val system = System(state)
+
+
+    val solution = when (solutionName) {
+        "trace" -> {
+            val traceFile = File("traces/$targetModelName.nbt").inputStream()
+            val commands: MutableList<Command> = mutableListOf()
+            while (traceFile.available() != 0) {
+                commands += Command.read(traceFile)
+            }
+            Trace(commands)
+        }
+        else -> throw IllegalArgumentException()
+
+    }
+    solution.apply(system)
+
+    val resultTraceFie = "results/${targetModelName}_$solutionName.nbt"
+    val ofile = FileOutputStream(File(resultTraceFie))
+    system.commandTrace.forEach { it.write(ofile) }
+
+    println(system.currentState.matrix == targetModel)
+    results.addNewResult(targetModelName, solutionName, system.currentState.energy.toLong(), resultTraceFie)
+
+    val writer = resultsFile.writer()
+    writer.write(results.toJson())
+    writer.flush()
 }
