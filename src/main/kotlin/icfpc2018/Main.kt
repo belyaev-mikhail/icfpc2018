cb1f45985fcca3aaa64265d9257226c416d0f532
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-07-22 16:01:48 +0300

    merge

--- src/main/kotlin/icfpc2018/Main.kt
+++ src/main/kotlin/icfpc2018/Main.kt
@@ -116,11 +116,66 @@ fun assemble(solutionName: String, targetModels: List<String>, resultsDir: Strin
     results.writeToDirectory(resultsDir)
 }
 
+fun disassemble(solutionName: String, targetModels: List<String>, resultsDir: String) {
+    val results = Results.readFromDirectory(resultsDir)
+    for (targetModelName in targetModels) {
+        log.info("Running with model $targetModelName")
+        val targetModelFile = File("models/${targetModelName}_src.mdl").inputStream()
+        val targetModel = Model.readMDL(targetModelFile)
+
+        val model = Model(targetModel.size)
+        val bot = Bot(1, Point(0, 0, 0), PersistentTreeSet.of(2..Config.maxBots))
+        val state = State(0, Harmonics.LOW, model, persistentTreeSetOf(bot))
+
+        val assembleSystem = System(state)
+        val solution = when (solutionName) {
+            "trace" -> {
+                val traceFile = File("traces/$targetModelName.nbt").inputStream()
+                val commands: MutableList<Command> = mutableListOf()
+                while (traceFile.available() != 0) {
+                    commands += Command.read(traceFile)
+                }
+                Trace(commands, assembleSystem)
+            }
+            else -> getSolutionByName(solutionName, targetModel, assembleSystem)
+        }
+        log.info(solution::class.java.name)
+
+        try {
+            solution.solve()
+        } catch (e: Exception) {
+            log.error("Solution $solution throwed exception $e")
+            continue
+        }
+
+        val resultingTrace = Trace(assembleSystem.commandTrace, System(state)).inverted()
+
+        log.info { "Energy: " + assembleSystem.currentState.energy }
+
+        val success = assembleSystem.currentState.matrix == targetModel
+
+
+        log.info(if (success) "Success" else "Fail")
+
+        if (success) {
+            val resultTraceFile = "$resultsDir/${targetModelName}_$solutionName.nbt"
+            results.addNewResult(targetModelName, solutionName, assembleSystem.currentState.energy, resultTraceFile)
+
+            val ofile = FileOutputStream(File(resultTraceFile))
+            resultingTrace.forEach { it.write(ofile) }
+            log.info("Results dumped")
+        }
+    }
+
+    results.writeToDirectory(resultsDir)
+}
+
 fun main(args: Array<String>) {
     val arguments = Arguments(args)
 
     when (arguments.getMode()) {
         RunMode.ASSEMBLE -> assemble(arguments.getSolution(), arguments.getModels(), arguments.getResults())
+        RunMode.DISASSEMBLE -> disassemble(arguments.getSolution(), arguments.getModels(), arguments.getResults())
         RunMode.SUBMIT -> submit(arguments.getSubmitDirectories())
         else -> throw IllegalArgumentException()
     }
