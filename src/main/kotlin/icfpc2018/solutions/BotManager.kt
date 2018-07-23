4514dbcbca5a8a23ae6dfae35dbfaa1bb7690d1b
sergei <hacker.mad.cat@gmail.com>
2018-07-23 03:53:33 +0300

    some fixes, added todos

--- src/main/kotlin/icfpc2018/solutions/BotManager.kt
+++ src/main/kotlin/icfpc2018/solutions/BotManager.kt
@@ -1,8 +1,6 @@
 package icfpc2018.solutions
 
-import icfpc2018.bot.commands.Command
-import icfpc2018.bot.commands.Flip
-import icfpc2018.bot.commands.Wait
+import icfpc2018.bot.commands.*
 import icfpc2018.bot.state.Bot
 import icfpc2018.bot.state.GroundError
 import icfpc2018.bot.state.Harmonics
@@ -11,12 +9,12 @@ import java.util.*
 import kotlin.coroutines.experimental.SequenceBuilder
 import kotlin.coroutines.experimental.buildIterator
 
-typealias Task = Iterator<Map<Bot, Command>>
+typealias Task = Iterator<Map<Int, Command>>
 
-data class TaggedTask(val tag: String, val inner: Task): Task by inner {
+data class TaggedTask(val tag: String, val inner: Task) : Task by inner {
     override fun toString() = "$tag"
 
-    constructor(tag: String, innerBuilder: suspend SequenceBuilder<Map<Bot, Command>>.() -> Unit) :
+    constructor(tag: String, innerBuilder: suspend SequenceBuilder<Map<Int, Command>>.() -> Unit) :
             this(tag, buildIterator(innerBuilder))
 }
 
@@ -45,7 +43,7 @@ class BotManager(val system: System) {
     }
 
     fun timeStep() {
-        val commands = TreeMap<Bot, Command>()
+        val commands = TreeMap<Int, Command>()
         val done = ArrayList<Task>()
         for (task in taskPool) {
             if (!task.hasNext()) {
@@ -54,7 +52,7 @@ class BotManager(val system: System) {
             }
             val taskCommands = task.next()
             for (bot in botPool) {
-                if (bot in taskCommands)
+                if (bot.id in taskCommands)
                     throw CollisionError()
             }
             if (taskCommands.keys.any { it in commands })
@@ -65,29 +63,34 @@ class BotManager(val system: System) {
             taskPool.remove(task)
         }
         for (bot in botPool) {
-            if (bot in commands)
+            if (bot.id in commands)
                 throw CollisionError()
+//            ToDo(Mikhail) wtf??
 //            if(botPool.size != system.numBots) commands[bot] = Wait
-            commands[bot] = Wait
+            commands[bot.id] = Wait
         }
+
         if (commands.values.all { it === Wait })
             ++fullWaitCounter
         else
             fullWaitCounter = 0
         if (fullWaitCounter == 3)
             throw DeadLockError()
-        system.timeStep(commands.values.toList())
 
-//        if (commands.values.all { it === Wait }) return
-
-        if(commands.isNotEmpty()) try {
-            if(system.currentState.harmonics == Harmonics.HIGH && system.currentState.matrix.isEverybodyGrounded) {
-                system.timeStep(commands.map { if(it.key == commands.keys.first()) Flip else Wait })
+        if (commands.isNotEmpty() /*&& commands.values.any { it is GFill || it is Fill } */) {
+            val timeStamp = system.timeStamp()
+            try {
+                if (system.currentState.harmonics == Harmonics.HIGH && system.currentState.matrix.isEverybodyGrounded) {
+                    system.timeStep(commands.map { if (it.key == commands.keys.first()) Flip else Wait })
+                }
+                system.timeStep(commands.values.toList())
+            } catch (ex: GroundError) {
+                system.rollBackTo(timeStamp)
+                if (system.currentState.harmonics == Harmonics.LOW) {
+                    system.timeStep(commands.map { if (it.key == commands.keys.first()) Flip else Wait })
+                }
+                system.timeStep(commands.values.toList())
             }
-            system.timeStep(commands.values.toList())
-        } catch (ex: GroundError) {
-            system.timeStep(commands.map { if(it.key == commands.keys.first()) Flip else Wait })
-            system.timeStep(commands.values.toList())
         }
     }
 
--- src/main/kotlin/icfpc2018/solutions/regions/Tasks.kt
+++ src/main/kotlin/icfpc2018/solutions/regions/Tasks.kt
@@ -65,7 +65,7 @@ fun <T> Iterator<T>.zipWithDefault(default: T, defaults: List<T>, other: List<It
 
 object RectangleTask {
     operator fun invoke(rectangle: Rectangle, manager: BotManager): Task = TaggedTask("$rectangle") {
-        val nothing = emptyMap<Bot, Command>()
+        val nothing = emptyMap<Int, Command>()
         val bots = doWhileNotNull(nothing) { manager.reserve(4) }
         val diff = NearCoordDiff(0, -1, 0)
         val p1 = rectangle.p1 + -diff // best code
@@ -73,21 +73,21 @@ object RectangleTask {
         val p3 = rectangle.p3 + -diff // best code
         val p4 = rectangle.p4 + -diff // best code
         val points = listOf(p1, p2, p3, p4)
-        val goto: List<Task> = bots.zip(points).map { (bot, p) -> GoTo(bot, p, manager) }
-        val default = mapOf(bots.first() to Wait)
-        val defaults: List<Map<Bot, Command>> = bots.drop(1).map { mapOf(it to Wait) }
+        val goto: List<Task> = bots.zip(points).map { (bot, p) -> GoTo(bot.id, bot.position, p, manager) }
+        val default = mapOf(bots.first().id to Wait)
+        val defaults = bots.drop(1).map { mapOf(it.id to Wait) }
         goto.first().zipWithDefault(default, defaults, goto.drop(1)).asSequence()
-                .map { it.fold(emptyMap<Bot, Command>()) { acc, m -> acc + m } }
+                .map { it.fold(emptyMap<Int, Command>()) { acc, m -> acc + m } }
                 .forEach { yield(it) }
         val diff1 = (rectangle.p3 - rectangle.p1).toFarCoordDiff()
         val diff2 = (rectangle.p4 - rectangle.p2).toFarCoordDiff()
         val diff3 = (rectangle.p1 - rectangle.p3).toFarCoordDiff()
         val diff4 = (rectangle.p2 - rectangle.p4).toFarCoordDiff()
         val diffs = listOf(diff1, diff2, diff3, diff4)
-        while(!manager.system.reserve(rectangle.points)) {
-            yield(bots.map { it to Wait }.toMap())
+        while (!manager.system.reserve(rectangle.points)) {
+            yield(bots.map { it.id to Wait }.toMap())
         }
-        val commands = bots.zip(diffs).map { (bot, d) -> bot to GFill(diff, d) }.toMap()
+        val commands = bots.zip(diffs).map { (bot, d) -> bot.id to GFill(diff, d) }.toMap()
         yield(commands)
         manager.release(bots)
     }
@@ -95,37 +95,39 @@ object RectangleTask {
 
 object SectionTask {
     operator fun invoke(section: Section, manager: BotManager): Task = TaggedTask("$section") {
-        val nothing = emptyMap<Bot, Command>()
+        val nothing = emptyMap<Int, Command>()
         val (bot1, bot2) = doWhileNotNull(nothing) { manager.reserve(2) }
         val diff = NearCoordDiff(0, -1, 0)
         val first = section.first + -diff // best code
         val second = section.second + -diff // best code
-        val goto1 = GoTo(bot1, first, manager)
-        val goto2 = GoTo(bot2, second, manager)
-        val default1 = mapOf(bot1 to Wait)
-        val default2 = mapOf(bot2 to Wait)
+        val goto1 = GoTo(bot1.id, bot1.position, first, manager)
+        val goto2 = GoTo(bot2.id, bot2.position, second, manager)
+        val default1 = mapOf(bot1.id to Wait)
+        val default2 = mapOf(bot2.id to Wait)
         goto1.zipWithDefault(default1, default2, goto2).asSequence()
                 .map { (c1, c2) -> c1 + c2 }
                 .forEach { yield(it) }
         val diff1 = (section.second - section.first).toFarCoordDiff()
         val diff2 = (section.first - section.second).toFarCoordDiff()
-        while(!manager.system.reserve(section.points)) {
-            yield(mapOf(bot1 to Wait, bot2 to Wait))
+        while (!manager.system.reserve(section.points)) {
+            yield(mapOf(bot1.id to Wait, bot2.id to Wait))
         }
-        yield(mapOf(bot1 to GFill(diff, diff1), bot2 to GFill(diff, diff2)))
+        manager.system.currentState.bots.filter { it.id in setOf(bot1.id, bot2.id) }.forEach { println(it) }
+        println("${bot1.id} $first ${bot2.id} $second $diff $diff1 $diff2")
+        yield(mapOf(bot1.id to GFill(diff, diff1), bot2.id to GFill(diff, diff2)))
         manager.release(listOf(bot1, bot2))
     }.iterator()
 }
 
 object VoxelTask {
     operator fun invoke(voxel: Voxel, manager: BotManager): Task = TaggedTask("$voxel") {
-        val nothing = emptyMap<Bot, Command>()
+        val nothing = emptyMap<Int, Command>()
         val (bot) = doWhileNotNull(nothing) { manager.reserve(1) }
         val diff = NearCoordDiff(0, -1, 0)
-        val goto = GoTo(bot, voxel.point + -diff, manager)
+        val goto = GoTo(bot.id, bot.position, voxel.point + -diff, manager)
         goto.forEach { yield(it) }
-        while(!manager.system.reserve(voxel.points)) yield(mapOf(bot to Wait))
-        yield(mapOf(bot to Fill(diff)))
+        while (!manager.system.reserve(voxel.points)) yield(mapOf(bot.id to Wait))
+        yield(mapOf(bot.id to Fill(diff)))
         manager.release(listOf(bot))
     }.iterator()
 }
@@ -133,13 +135,13 @@ object VoxelTask {
 fun <T> List<T>.sepWhile(limit: Int = size, predicate: (T) -> Boolean): Pair<List<T>, List<T>> {
     val before = mutableListOf<T>()
     var j = 0
-    for(i in 0..minOf(lastIndex, limit - 1)) {
+    for (i in 0..minOf(lastIndex, limit - 1)) {
         j = i
         val it = get(i)
-        if(predicate(it)) before += it
+        if (predicate(it)) before += it
         else return before to this.subList(i, size)
     }
-    if(before.size == size) return before to listOf()
+    if (before.size == size) return before to listOf()
     return before to subList(before.size, size)
 }
 
@@ -162,22 +164,21 @@ object GoTo {
 
         require(
                 diffs.fold(trace.first()) { p, d -> p + d } == trace.last()
-        ){"Small diffs messed up =("}
+        ) { "Small diffs messed up =(" }
 
         var current = diffs
 
         var forceShort = false
 
-        while(current.isNotEmpty()) {
+        while (current.isNotEmpty()) {
             val pt = current.first()
             val (move, rest) = current.sepWhile(limit = 15) { it.axis == pt.axis }
             current = rest
             if (move.isEmpty()) continue
             if (move.size > 5 && !forceShort) {
                 res += move.reduce<CoordDiff, CoordDiff> { a, b -> a + b }.toLong()
-            }
-            else {
-                if(move.size > 5) {
+            } else {
+                if (move.size > 5) {
                     res += move.take(5).reduce<CoordDiff, CoordDiff> { a, b -> a + b }.toShort()
                     res += move.drop(5).reduce<CoordDiff, CoordDiff> { a, b -> a + b }.toLong()
                 } else {
@@ -189,15 +190,15 @@ object GoTo {
 
         require(
                 res.fold(trace.first()) { p, d -> p + d } == trace.last()
-        ){"Big diffs messed up =("}
+        ) { "Big diffs messed up =(" }
 
         val it = res.iterator()
         val commands = mutableListOf<Command>()
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             val e = it.next()
-            if(e is LongCoordDiff) commands += SMove(e)
-            if(e is ShortCoordDiff) {
-                if(it.hasNext()) {
+            if (e is LongCoordDiff) commands += SMove(e)
+            if (e is ShortCoordDiff) {
+                if (it.hasNext()) {
                     val e2 = it.next() as? ShortCoordDiff ?: throw IllegalArgumentException()
                     commands += LMove(e, e2)
                 } else {
@@ -208,15 +209,15 @@ object GoTo {
         return commands
     }
 
-    operator fun invoke(bot: Bot, point: Point, manager: BotManager): Task = TaggedTask("Goto($bot, $point)") {
-        if (bot.position == point) return@TaggedTask
+    operator fun invoke(bid: Int, from: Point, to: Point, manager: BotManager): Task = TaggedTask("Goto(Bot(id=$bid), $to)") {
+        if (from == to) return@TaggedTask
         val system = manager.system
-        val wait = mapOf(bot to Wait)
-        val trace = doWhileNotNull(wait) { buildTrace(bot.position, point, system) }
+        val wait = mapOf(bid to Wait)
+        val trace = doWhileNotNull(wait) { buildTrace(from, to, system) }
         if (!system.lightReserve(trace))
             throw IllegalStateException("Cannot reserve")
         for (command in convertTrace(trace)) {
-            yield(mapOf(bot to command))
+            yield(mapOf(bid to command))
         }
         system.release(trace)
     }
@@ -226,15 +227,15 @@ private operator fun CoordDiff.plus(b: CoordDiff): CoordDiff =
         CoordDiff(dx + b.dx, dy + b.dy, dz + b.dz)
 
 object GoToBase {
-    operator fun invoke(manager: BotManager): Task =  TaggedTask("GotoBase()") {
+    operator fun invoke(manager: BotManager): Task = TaggedTask("GotoBase()") {
         val numBots = manager.system.numBots
         val points = Pair(Point.ZERO, Point(numBots, 0, 0)).coords()
-        val bots =  manager.reserve(numBots) ?: throw IllegalStateException("WTF")
-        val goto: List<Task> = bots.zip(points).map { (bot, p) -> GoTo(bot, p, manager) }
-        val default = mapOf(bots.first() to Wait)
-        val defaults: List<Map<Bot, Command>> = bots.drop(1).map { mapOf(it to Wait) }
+        val bots = manager.reserve(numBots) ?: throw IllegalStateException("WTF")
+        val goto: List<Task> = bots.zip(points).map { (bot, p) -> GoTo(bot.id, bot.position, p, manager) }
+        val default = mapOf(bots.first().id to Wait)
+        val defaults = bots.drop(1).map { mapOf(it.id to Wait) }
         val subs = goto.first().zipWithDefault(default, defaults, goto.drop(1)).asSequence()
-                .map { it.fold(emptyMap<Bot, Command>()) { acc, m -> acc + m } }
+                .map { it.fold(emptyMap<Int, Command>()) { acc, m -> acc + m } }
         yieldAll(subs)
         manager.release(bots)
     }.iterator()
